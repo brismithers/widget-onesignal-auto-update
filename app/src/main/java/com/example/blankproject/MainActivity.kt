a8c5678cbe46e8790b55c77d4038ee7463925d0e
Brian Smith <briansmith@onesignal.com>
2023-01-24 16:41:37 -0500

    Add button to main activity. This button will schedule work via androidx.work.  Whenever work is scheduled, or run, it will drive the widget onUpdate to be called.  See issue https://issuetracker.google.com/issues/115575872.

--- app/src/main/java/com/example/blankproject/MainActivity.kt
+++ app/src/main/java/com/example/blankproject/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.example.blankproject
 
 import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.widget.Button
+import androidx.work.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : Activity() {
+    private var scheduleWorkButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        scheduleWorkButton = findViewById(R.id.scheduleWorkBtn)
+        scheduleWorkButton!!.setOnClickListener {
+            val constraints = Constraints.Builder()
+                                         .setRequiredNetworkType(NetworkType.CONNECTED)
+                                         .build()
+            val workRequest = OneTimeWorkRequest.Builder(ScheduledWorker::class.java)
+                .setConstraints(constraints)
+                .setInitialDelay(10, TimeUnit.SECONDS)
+                .addTag("SCHEDULE_TAG")
+                .build()
+
+            WorkManager.getInstance(applicationContext)
+                .enqueueUniqueWork(
+                    "SCHEDULE_TAG",
+                    ExistingWorkPolicy.KEEP,
+                    workRequest
+                )
+        }
+    }
+
+    class ScheduledWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+        override fun doWork(): Result {
+            return Result.success()
+        }
     }
 }
--- app/src/main/java/com/example/blankproject/MainApplication.kt
+++ app/src/main/java/com/example/blankproject/MainApplication.kt
@@ -8,9 +8,9 @@ class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
-        OneSignal.initWithContext(this)
-        OneSignal.setAppId("YOUR-APP-ID")
+//        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+//        OneSignal.initWithContext(this)
+//        OneSignal.setAppId("YOUR-APP-ID")
     }
 
 
